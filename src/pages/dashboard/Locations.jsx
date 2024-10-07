d3224f64c1571f2b3a7a976c4dfe08d4b648633a
sebasx200 <sebasx200@hotmail.com>
2024-10-07 16:44:01 -0500

    refactor: change locations background to be the same of the rest of the app

--- src/pages/dashboard/Locations.jsx
+++ src/pages/dashboard/Locations.jsx
@@ -1,9 +1,12 @@
 import MapLocations from "../../components/dashboard/MapLocations";
+import styles from "./Dashboard.module.css";
 
 function Locations() {
   return (
-    <div>
-      <MapLocations />
+    <div className={styles.backgroundPages}>
+      <div className={`container mt-5 ${styles.agendaPanel}`}>
+        <MapLocations />
+      </div>
     </div>
   );
 }
